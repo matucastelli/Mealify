cb51aac53335658a419ecb3bf0daaec3f7188ba6
matucastelli <matucastelli2006@gmail.com>
2026-08-23 19:20:02 -0300

    fix: Entorno local de vercel y actualizacion a Axios

--- api/buscar.js
+++ api/buscar.js
@@ -1,12 +1,14 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
     const { query } = req.query;
 
     try {
         const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${process.env.SPOONACULAR_KEY}`;
-        const respuesta = await fetch(url);
-        const datos = await respuesta.json();
-        res.status(200).json(datos);
+        const respuesta = await axios.get(url);
+        res.status(200).json(respuesta.data);
     } catch {
+        console.error("EL ERROR REAL ES:", error);
         res.status(500).json({ error: "Hubo un error al buscar recetas" }); 
     }
 }
--- api/random.js
+++ api/random.js
@@ -1,12 +1,15 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
     const { cantidad } = req.query;
 
     try {
+        console.log("LA CLAVE QUE LEE NODE ES:", process.env.SPOONACULAR_KEY);
         const url = `https://api.spoonacular.com/recipes/random?number=${cantidad}&apiKey=${process.env.SPOONACULAR_KEY}`;
-        const respuesta = await fetch(url);
-        const datos = await respuesta.json();
-        res.status(200).json(datos);
-    } catch {
+        const respuesta = await axios.get(url);
+        res.status(200).json(respuesta.data);
+    } catch (error) {
+        console.error("EL ERROR REAL ES:", error);
         res.status(500).json({ error: "Hubo un error al obtener recetas" });
     }
 }
